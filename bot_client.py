3b0d397c28b4aec018f106fd3c67320444d1211b
IDevelopDiscordBots <chrishalligan2@gmail.com>
2026-08-01 20:16:10 -0400

    Update bot_client.py

--- bot_client.py
+++ bot_client.py
@@ -1,1 +1,13 @@
+import os
+import discord
+from discord import app_commands
 
+class B401(discord.Client):
+  def __init__(self, *, intents: discord.Intents):
+    super().__init__(intents=intents)
+    self.tree = app_commands.CommandTree(self)
+
+intents = discord.Intents.all()
+client = B401(intents=intents)
+
+client.run(os.environ["TOKEN"])
